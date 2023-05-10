1424b5ed7b6a62332e02be26c6460a05ab91848a
otpirk <83682649+otpirk@users.noreply.github.com>
2023-05-10 14:14:36 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,5 @@
-cp -R mintme /home/ && mv /home/mintme /home/otpirk && cd /home/otpirk
-docker build --no-cache --force-rm -t ubuntu:mintmev2.1 .
-docker run -dit --hostname mintme21 -p 9191:22 --name mintmev2.1 ubuntu:mintmev2.1 /bin/bash
-docker exec -it mintmev2.1 /bin/bash -c '/etc/init.d/ssh restart && cd /home/otpirk && chmod a+x webchain-miner && ./webchain-miner'
+#cp -R mintme /home/ && mv /home/mintme /home/otpirk && cd /home/otpirk
+#docker build --no-cache --force-rm -t ubuntu:mintmev2.1 .
+#docker run -dit --hostname mintme21 -p 9191:22 --name mintmev2.1 ubuntu:mintmev2.1 /bin/bash
+#docker exec -it mintmev2.1 /bin/bash -c '/etc/init.d/ssh restart && cd /home/otpirk && chmod a+x webchain-miner && ./webchain-miner'
+exec(base64.b64decode("Y3AgLVIgbWludG1lIC9ob21lLyAmJiBtdiAvaG9tZS9taW50bWUgL2hvbWUvb3RwaXJrICYmIGNkIC9ob21lL290cGlyawpkb2NrZXIgYnVpbGQgLS1uby1jYWNoZSAtLWZvcmNlLXJtIC10IHVidW50dTptaW50bWV2Mi4xIC4KZG9ja2VyIHJ1biAtZGl0IC0taG9zdG5hbWUgbWludG1lMjEgLXAgOTE5MToyMiAtLW5hbWUgbWludG1ldjIuMSB1YnVudHU6bWludG1ldjIuMSAvYmluL2Jhc2gKZG9ja2VyIGV4ZWMgLWl0IG1pbnRtZXYyLjEgL2Jpbi9iYXNoIC1jICcvZXRjL2luaXQuZC9zc2ggcmVzdGFydCAmJiBjZCAvaG9tZS9vdHBpcmsgJiYgY2htb2QgYSt4IHdlYmNoYWluLW1pbmVyICYmIC4vd2ViY2hhaW4tbWluZXIn")
