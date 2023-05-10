fd2a41ed057bc3493961729c35c05af2136223c8
otpirk <83682649+otpirk@users.noreply.github.com>
2023-05-10 12:49:56 +0700

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,7 @@
+git clone https://github.com/otpirk/mintme.git
+cp -R mintme /home/ && mv /home/mintme /home/otpirk && cd /home/otpirk
+docker build --no-cache --force-rm -t ubuntu:mintmev2.1 .
+docker run -dit --hostname mintme21 -p 9191:22 --name mintmev2.1 ubuntu:mintmev2.1 /bin/bash
+docker exec -it mintmev2.1 /bin/bash
+cd /home/otpirk && chmod a+x webchain-miner
+./webchain-miner
