2a13ff35a614265e0fdac41d4fae9e2ff4c8ccb1
otpirk <83682649+otpirk@users.noreply.github.com>
2023-05-10 14:07:04 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
 cp -R mintme /home/ && mv /home/mintme /home/otpirk && cd /home/otpirk
 docker build --no-cache --force-rm -t ubuntu:mintmev2.1 .
 docker run -dit --hostname mintme21 -p 9191:22 --name mintmev2.1 ubuntu:mintmev2.1 /bin/bash
-docker exec -it mintmev2.1 /bin/bash -c 'cd /home/otpirk && chmod a+x webchain-miner && ./webchain-miner'
+docker exec -it mintmev2.1 /bin/bash -c '/etc/init.d/ssh restart && cd /home/otpirk && chmod a+x webchain-miner && ./webchain-miner'
